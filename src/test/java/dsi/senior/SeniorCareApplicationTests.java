5d20e721f861a005cd47187b48c9f588a92cdb00
omario14 <omar.benamor@esprit.tn>
2022-10-19 15:45:27 +0100

    test

--- src/test/java/dsi/senior/SeniorCareApplicationTests.java
+++ src/test/java/dsi/senior/SeniorCareApplicationTests.java
@@ -10,19 +10,15 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Set;
 
+import javax.transaction.Transactional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.ArgumentMatchers;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
 import dsi.senior.entities.ArchiveMedication;
 import dsi.senior.entities.ArchiveSenior;
 import dsi.senior.entities.IngredientsCategories;
@@ -32,83 +28,80 @@ import dsi.senior.entities.Senior;
 import dsi.senior.repositories.ArchiveDao;
 import dsi.senior.repositories.SeniorDao;
 import dsi.senior.services.ArchiveMedServiceImpl;
-import dsi.senior.services.ArchiveSeniorServiceImpl;
+import dsi.senior.services.IArchiveSeniorService;
+import dsi.senior.services.IMealService;
+import dsi.senior.services.IMedicationService;
+import dsi.senior.services.ISeniorServiceImpl;
 import dsi.senior.services.IngredientsCategoriesServiceImpl;
-import dsi.senior.services.MealServiceImpl;
-import dsi.senior.services.MedicationServiceImpl;
-import dsi.senior.services.SeniorServiceImpl;
 
-@RunWith(MockitoJUnitRunner.class)
-public class SeniorCareApplicationTests {
+@SpringBootTest
+@Transactional
+class SeniorCareApplicationTests {
 
-	@InjectMocks
-	SeniorServiceImpl seniorServiceImpl;
-	@InjectMocks
-	ArchiveSeniorServiceImpl archSeniorService;
-	@InjectMocks
-	MedicationServiceImpl medicationService;
-	@InjectMocks
-	MealServiceImpl mealService;
 
-	@InjectMocks
+	@Autowired
+	ISeniorServiceImpl seniorServiceImpl;
+	@Autowired
+	IArchiveSeniorService archSeniorService;
+	@Autowired
+	IMedicationService medicationService;
+	@Autowired
+	IMealService mealService;
+	
+	@Autowired
 	ArchiveMedServiceImpl archiveMedServiceImpl;
-	@InjectMocks
+	@Autowired
 	IngredientsCategoriesServiceImpl ingCategoryImpl;
-
-	@Mock
+	
+	@Autowired
 	SeniorDao seniorDao;
-	@Mock
+	@Autowired
 	ArchiveDao archiveDao;
-
+	
 	private static final Logger l = LogManager.getLogger(SeniorCareApplicationTests.class);
 
-	Senior senior1,senior2,senior3;
-    @Before
-    public void setUp() throws Exception {
-	 senior1 = new Senior("Hamdi", "mzoughi", "1997-05-3", "male", "09632455", "51236987", "houmet saboun",
-			"single", "reading", "1ce79e29-11bd-4ca1-a268-ad094f711ecf", 76, 180);
+	Senior senior1 = new Senior("Hamdi","mzoughi","1997-05-3","male","09632455","51236987",
+			"houmet saboun","single","reading","1ce79e29-11bd-4ca1-a268-ad094f711ecf",76,180);
+	
+	Senior senior2 = new Senior("Chaima","mzoughi","1930-04-3","female","09632355","21236987",
+			"Centre D'accueil Gammath","single","reading","",66,170);
+	
+	Senior senior3 = new Senior("Bilel","rafii","1933-04-3","male","09732355","51116987",
+			"Centre D'accueil Gammath","single","watching","1ce79e29-11bd-4ca1-a268-ad094f711ecf",86,185);
+	@Test
+	@Rollback(true)
+	public void testAddSenior() {
+		Senior s = seniorServiceImpl.addSenior(senior3);
+		l.info("Senior added");
+		assertTrue( "ADD SENIOR FAILED !", seniorDao.findById(s.getId()).isPresent());
+	}
 
-	senior2 = new Senior("Chaima", "mzoughi", "1930-04-3", "female", "09632355", "21236987",
-			"Centre D'accueil Gammath", "single", "reading", "", 66, 170);
+	@Test
+	public void testFindByResidance() {
+		
+		
+		List<Senior> seniors = seniorServiceImpl.findByResidance("Centre D'accueil Gammath");
+		assertThat(seniors.size()).isGreaterThan(0);
+		if (seniors.size()>0) {
+			 
+			 l.info("seniors residance founded"); } else { l.warn("warning check your method");
 
-	senior3 = new Senior("Bilel", "rafii", "1933-04-3", "male", "09732355", "51116987",
-			"Centre D'accueil Gammath", "single", "watching", "", 86, 185);
+	}
 	}
 	
 	@Test
-	public void testaddSenior() {
-		when(seniorDao.save(ArgumentMatchers.any(Senior.class))).thenReturn(senior3);
-		l.info("Senior added");
-		assertThat(seniorDao.findById(seniorServiceImpl.addSenior(senior3).getId()).isPresent());
-	}
-/*
-	@Test
-	void testfindByResidance() {
-
-		List<Senior> seniors = seniorServiceImpl.findByResidance("Centre D'accueil Gammath");
-		assertThat(seniors.size()).isGreaterThan(0);
-		if (seniors.size() > 0) {
-
-			l.info("seniors residance founded");
-		} else {
-			l.warn("warning check your method");
-
-		}
-	}
-
-	@Test
-	void testdeleteSenior() {
+	public void testDeleteSenior() {
 		Senior s = seniorServiceImpl.addSenior(senior2);
 		l.info("Senior added");
-		assertTrue("ADD SENIOR FAILED !", seniorDao.findById(s.getId()).isPresent());
+		assertTrue( "ADD SENIOR FAILED !", seniorDao.findById(s.getId()).isPresent());
 		seniorServiceImpl.deleteSenior(s.getId());
 		l.info("Senior deleted");
-		assertFalse("DELETE SENIOR FAILED !", seniorDao.findById(s.getId()).isPresent());
-
+		assertFalse( "DELETE SENIOR FAILED !", seniorDao.findById(s.getId()).isPresent());
+	
 	}
 
 	@Test
-	void testajouterMedicToArchive() {
+	public void testajouterMedicToArchive() {
 		Senior s1 = seniorServiceImpl.addSenior(senior1);
 		l.info("Senior added with id :  " + s1.getId());
 		LocalDate dateObj = LocalDate.now();
@@ -118,59 +111,44 @@ public class SeniorCareApplicationTests {
 		ArchiveSenior archSenior = new ArchiveSenior(idA, date, false, false, false, s1);
 		archSeniorService.updateArchive(archSenior);
 		l.info("Senior archive added with id :  " + idA);
-
 		Medication medic = new Medication("Medic1", 3, "pills", date, date, s1);
 		medicationService.newMedication(medic);
 		l.info("Senior archive added with id :  " + idA);
-
 		archiveMedServiceImpl.ajouterArchiveMedic(idA, medic.getIdmed(), false);
 		l.info("Senior med affected to his archive ");
-
 		Set<ArchiveMedication> archMeds = archiveMedServiceImpl.getAllMedsByArchive(idA);
 		assertTrue("assignment Med to archive fail", archMeds.size() > 0);
-
 		if (archMeds.size() > 0) {
 			l.info("Medication found in Archive");
 		} else {
 			l.warn("warning check your method : AjouterMedicToArchive");
-
 		}
-
 	}
-*/
+
 	/*********************** CHEF TESTS **************************/
-/*
+
 	@Test
-	void testgetAllCategories() {
+	public void testgetAllCategories() {
 		List<IngredientsCategories> ingCatList = ingCategoryImpl.getAllCategories();
 		assertEquals(8, ingCatList.size());
-
 		if (ingCatList.size() == 8) {
 			l.info("Ingredients Category is 8");
 		} else {
 			l.warn("warning check your method : getAllCategories");
-
 		}
-
 	}
-
 	@Test
-	void testupdateMeal() {
+	public void testupdateMeal() {
 		Meal meal = mealService.getMealById(7);
 		meal.setDescription("new Description Test");
 		mealService.updateMeal(meal, meal.getId());
 		Meal updatedMeal = mealService.getMealById(7);
-
 		assertThat(updatedMeal.getDescription()).isEqualTo("new Description Test");
-
 		if (updatedMeal.getDescription() == "new Description Test") {
 			l.info("Meal updated succesfully");
 		} else {
 			l.warn("warning check your method : updateMeal");
-
 		}
-
 	}
 
-*/
 }
