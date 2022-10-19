15647d546a54eba10bef2369cd110b94d0c1f09f
omario14 <omar.benamor@esprit.tn>
2022-10-19 16:40:03 +0100

    test

--- src/test/java/dsi/senior/SeniorCareApplicationTests.java
+++ src/test/java/dsi/senior/SeniorCareApplicationTests.java
@@ -36,7 +36,6 @@ import dsi.senior.services.IngredientsCategoriesServiceImpl;
 
 @RunWith(Runner.class)
 @SpringBootTest
-@Transactional
 public class SeniorCareApplicationTests {
 
 
@@ -62,15 +61,14 @@ public class SeniorCareApplicationTests {
 	private static final Logger l = LogManager.getLogger(SeniorCareApplicationTests.class);
 
 	Senior senior1 = new Senior("Hamdi","mzoughi","1997-05-3","male","09632455","51236987",
-			"houmet saboun","single","reading","1ce79e29-11bd-4ca1-a268-ad094f711ecf",76,180);
+			"houmet saboun","single","reading","",76,180);
 	
 	Senior senior2 = new Senior("Chaima","mzoughi","1930-04-3","female","09632355","21236987",
 			"Centre D'accueil Gammath","single","reading","",66,170);
 	
-	Senior senior3 = new Senior("Bilel","rafii","1933-04-3","male","09732355","51116987",
-			"Centre D'accueil Gammath","single","watching","1ce79e29-11bd-4ca1-a268-ad094f711ecf",86,185);
+	Senior senior3 = new Senior("Bilel","Nafati","1933-04-3","male","09732355","51116987",
+			"Centre D'accueil Gammath","single","watching","",86,185);
 	@Test
-	@Rollback(true)
 	public void testAddSenior() {
 		Senior s = seniorServiceImpl.addSenior(senior3);
 		l.info("Senior added");
