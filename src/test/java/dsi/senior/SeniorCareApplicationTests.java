cc61d464ced36f82a3d32f6f80d85d2cd1c5b258
omario14 <omar.benamor@esprit.tn>
2022-10-19 16:15:19 +0100

    test

--- src/test/java/dsi/senior/SeniorCareApplicationTests.java
+++ src/test/java/dsi/senior/SeniorCareApplicationTests.java
@@ -1,7 +1,6 @@
 package dsi.senior;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -12,9 +11,8 @@ import javax.transaction.Transactional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Assert;
-import org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -73,7 +71,7 @@ public class SeniorCareApplicationTests {
 	public void testAddSenior() {
 		Senior s = seniorServiceImpl.addSenior(senior3);
 		l.info("Senior added");
-		assertTrue( "ADD SENIOR FAILED !", seniorDao.findById(s.getId()).isPresent());
+		Assertions.assertTrue(  seniorDao.findById(s.getId()).isPresent(),"ADD SENIOR FAILED !");
 	}
 
 	@Test
@@ -81,7 +79,7 @@ public class SeniorCareApplicationTests {
 		
 		
 		List<Senior> seniors = seniorServiceImpl.findByResidance("Centre D'accueil Gammath");
-		assertTrue( "seniors residance not founded", seniors.size()>0);
+		Assertions.assertTrue((seniors.size())>0,"seniors residance not founded");
 		if (seniors.size()>0) {
 			 
 			 l.info("seniors residance founded"); } else { l.warn("warning check your method");
@@ -89,4 +87,66 @@ public class SeniorCareApplicationTests {
 	}
 	}
 	
+	@Test
+	public void testDeleteSenior() {
+		Senior s = seniorServiceImpl.addSenior(senior2);
+		l.info("Senior added");
+		Assertions.assertTrue(  seniorDao.findById(s.getId()).isPresent(),"ADD SENIOR FAILED !");
+		seniorServiceImpl.deleteSenior(s.getId());
+		l.info("Senior deleted");
+		Assertions.assertFalse(  seniorDao.findById(s.getId()).isPresent(),"DELETE SENIOR FAILED !");
+	
+	}
+
+	@Test
+	public void testajouterMedicToArchive() {
+		Senior s1 = seniorServiceImpl.addSenior(senior1);
+		l.info("Senior added with id :  " + s1.getId());
+		LocalDate dateObj = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String date = dateObj.format(formatter);
+		String idA = "arch-" + s1.getId() + "-" + date;
+		ArchiveSenior archSenior = new ArchiveSenior(idA, date, false, false, false, s1);
+		archSeniorService.updateArchive(archSenior);
+		l.info("Senior archive added with id :  " + idA);
+		Medication medic = new Medication("Medic1", 3, "pills", date, date, s1);
+		medicationService.newMedication(medic);
+		l.info("Senior archive added with id :  " + idA);
+		archiveMedServiceImpl.ajouterArchiveMedic(idA, medic.getIdmed(), false);
+		l.info("Senior med affected to his archive ");
+		Set<ArchiveMedication> archMeds = archiveMedServiceImpl.getAllMedsByArchive(idA);
+		Assertions.assertTrue( archMeds.size() > 0,"assignment Med to archive fail");
+		if (archMeds.size() > 0) {
+			l.info("Medication found in Archive");
+		} else {
+			l.warn("warning check your method : AjouterMedicToArchive");
+		}
+	}
+
+	/*********************** CHEF TESTS **************************/
+
+	@Test
+	public void testgetAllCategories() {
+		List<IngredientsCategories> ingCatList = ingCategoryImpl.getAllCategories();
+		assertEquals(8, ingCatList.size());
+		if (ingCatList.size() == 8) {
+			l.info("Ingredients Category is 8");
+		} else {
+			l.warn("warning check your method : getAllCategories");
+		}
+	}
+	@Test
+	public void testupdateMeal() {
+		Meal meal = mealService.getMealById(7);
+		meal.setDescription("new Description Test");
+		mealService.updateMeal(meal, meal.getId());
+		Meal updatedMeal = mealService.getMealById(7);
+		Assertions.assertTrue((updatedMeal.getDescription())==("new Description Test"));
+		if (updatedMeal.getDescription() == "new Description Test") {
+			l.info("Meal updated succesfully");
+		} else {
+			l.warn("warning check your method : updateMeal");
+		}
+	}
+
 }
