f7de8da4e876ed8107872cdb904628af6d54679e
omario14 <omar.benamor@esprit.tn>
2022-10-28 10:39:20 +0100

    csrf enable

--- src/main/java/dsi/senior/cnf/WebSecurityConfig.java
+++ src/main/java/dsi/senior/cnf/WebSecurityConfig.java
@@ -63,7 +63,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
 		httpSecurity.cors();
-		httpSecurity.csrf().disable()
+		httpSecurity.csrf();/*.disable()
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/authenticate", "/signup","/addabc/**","/doseTimeDone/**","/updateSymptoms/**","/files/**","/retrieves-all-role", "/v3/api-docs/**","/swagger-ui/**","/configuration/ui","/swagger-resources/**","/configuration/security","/addSenior").permitAll().anyRequest().
 				// all other requests need to be authenticated
@@ -71,7 +71,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
 				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);*/
 
 		// Add a filter to validate the tokens with every request
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
